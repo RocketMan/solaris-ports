96b917a868dbbac08704f3c63c2e86e15e899a14
Jim Mason <jmason@ibinx.com>
2021-06-09 18:06:02 +0100

    added libdvdread build dependency

--- components/desktop/gstreamer/gst-plugins-bad/depend.mk
+++ components/desktop/gstreamer/gst-plugins-bad/depend.mk
@@ -1,1 +1,1 @@
-desktop/gstreamer/gst-plugins-bad: desktop/gstreamer/gstreamer desktop/gstreamer/gst-plugins-base media/libfdk-aac
+desktop/gstreamer/gst-plugins-bad: desktop/gstreamer/gstreamer desktop/gstreamer/gst-plugins-base media/libfdk-aac media/libdvdread
--- components/desktop/gstreamer/gst-plugins-ugly/depend.mk
+++ components/desktop/gstreamer/gst-plugins-ugly/depend.mk
@@ -1,1 +1,1 @@
-desktop/gstreamer/gst-plugins-ugly: desktop/gstreamer/gstreamer desktop/gstreamer/gst-plugins-base media/libx264
+desktop/gstreamer/gst-plugins-ugly: desktop/gstreamer/gstreamer desktop/gstreamer/gst-plugins-base media/libx264 media/libdvdread
